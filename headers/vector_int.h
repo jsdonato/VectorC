5072e2b9f160067f5210002b4509d2da57f65d2b
068432 <jsd982412@gmail.com>
2024-12-29 19:33:56 -0700

    some functionality

--- headers/vector_int.h
+++ headers/vector_int.h
@@ -6,17 +6,25 @@ struct vector_int {
 };
 
 void init(vector_int* vec) {
-  vec->data = (int *)malloc(1 * sizeof(int));
+  vec->data = (int*)malloc(1 * sizeof(int));
   size = 0;
   capacity = 1;
 }
 
 void clear(vector_int* vec) {
-  vec->data = nullptr;
+  free(vec->data);
   size = 0;
-  capacity = 0;
+  capacity = 1;
 }
 
 void push_back(vector_int* vec, int datum) {
-  
+  if (vec->size == vec->capacity) {
+    vec->data = (int*)realloc(vec->data, 2 * capacity * sizeof(int));
+    ++(vec->size());
+    vec->capacity *= 2;
+  }
+  else {
+    (vec->data)[size] = datum;
+    ++(vec->size);
+  }
 }

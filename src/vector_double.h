19b938ec69ec63e3eb7a6b4c97ce3b147ce07ed0
068432 <jsd982412@gmail.com>
2025-01-01 11:53:19 -0700

    headers for vector double

---
+++ src/vector_double.h
@@ -0,0 +1,23 @@
+#include "stdlib.h"
+
+typedef struct {
+  double* data;
+  int size;
+  int capacity;
+} vector_double;
+
+void init(vector_double* vec);
+
+void init_size(vector_double* vec, int size);
+
+void clear(vector_double* vec);
+
+void push_back(vector_double* vec, double datum);
+
+double at(vector_double* vec, int i);
+
+void set(vector_double* vec, int i, double datum);
+
+void set_size(vector_double* vec, int size);
+
+void set_capacity(vector_double* vec, int capacity);
